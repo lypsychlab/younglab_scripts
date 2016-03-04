179980694e4dfdb0b0bcc07c799dff084e3461fa
lypsychlab <lypsychlab@gmail.com>
2016-03-04 16:08:45 -0500

    fixed ieh_recode

--- ieh_recode_itemwise.m
+++ ieh_recode_itemwise.m
@@ -1,5 +1,5 @@
 study='IEHFMRI';
-subj_nums=[4:8 11:14 16:22 24 25]; % all subjects
+subj_nums=[5:8 11:14 16:22 24 25]; % all subjects
 subjs={};sessions={};
 for s=1:length(subj_nums)
     subjs{end+1}=['YOU_IEHFMRI_1' sprintf('%02d',subj_nums(s))];
@@ -21,7 +21,7 @@ for thissub=1:length(subjs)
 
 	cd(fullfile('/younglab/studies',study,subjs{thissub},'results/ieh_results_itemwise_normed'));
 	betadir=dir('beta_*.nii');
-	for thisbeta=1:length(betadir)
+	for thisbeta=1:length(betanames)
 		movefile(betadir(thisbeta).name,['beta_item_' num2str(thisbeta) '_' betanames{thisbeta}]);
 	end
 
